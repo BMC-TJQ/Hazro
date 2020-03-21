be91a58ec192c26d66d27a13cea59449dc24b3e0
Blackfish <58276684+BMC-TJQ@users.noreply.github.com>
2020-03-21 21:10:49 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,6 +1,6 @@
 /* Daftar Pengguna Template */
 var twLSN = [
-'hazrocookies.blogspot.com','www.hazrocookies.blogspot.com',
+'hazrocookies.blogspot.com','www.hazrocookies.blogspot.com','hazrocookies.com','www.hazrocookies.com',
 ];
 /* URL Pemilik Lisensi */
 var redirectURL = 'https://www.facebook.com/Muhadi.M.ilyas';
